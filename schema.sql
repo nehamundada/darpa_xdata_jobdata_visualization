b322dfc94cf99562ff9cffdbeccce4d938bfb05e
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-11-29 18:00:05 -0800

    added schema statements:

---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- scheam changes
+
+CREATE TABLE corporate_presence ( total int, company text, city text, country text);
+
+
+insert into corporate_presence
+select count(*), company, city, country from jobs join address_info on lat=latitude and long = longitude 
+where latitude !=0 and latitude != ' ' and latitude is not null
+group by company, city, country 
+order by count(*) desc;
+
+CREATE INDEX corporate_presence_idx_1 on corporate_presence(country);
+CREATE INDEX corporate_presence_idx_2 on corporate_presence(city);
+CREATE INDEX corporate_presence_idx_3 on corporate_presence(company);
+
+
+CREATE TABLE country_points (lat int, long int, country text, company text);
+select distinct latitude, longitude, country, company from jobs join address_info on lat=latitude and long=longitude;
