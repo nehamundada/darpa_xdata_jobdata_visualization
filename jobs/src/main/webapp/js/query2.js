79382f4a00210a05f917e0939dce0442b5ca3d7d
nehamundada <neha2.mundada@gmail.com>
2014-12-05 23:29:27 -0800

    added main page to wire the queries

--- jobs/src/main/webapp/js/query2.js
+++ jobs/src/main/webapp/js/query2.js
@@ -2,6 +2,7 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 	
 	$(document).ready(function(){
 		
+		$("#legend").css("display", "none");
 		$("#serachCompany").autocomplete({
 	      source: window.apiPath + 'serachCompany',
 	      minLength: 3,
@@ -18,7 +19,7 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 				datatype : 'json',
 				data : {'term' : $( "#serachCompany").val() },
 				success : function(data) {
-					
+					$("#legend").css("display", "block");
 					var mapPoints = [];
 					
 					var fillArr = {'defaultFill': '#EDDC4E', '2012' : "#d67777", '2013': "#4f99b4"};		
@@ -55,27 +56,12 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 						mapPoints.push(o2);
 					}
 					
-					
-					/*
-					var data2012 = [];
-					for(var x in data['2012']) {
-						data2012.push({'location' : data['2012'][x]});
-					}
-					var data2013 = [];
-					for(var x in data['2013']) {
-						data2013.push({'location' : data['2013'][x]});
-					}
-					console.log(data2012.length);
-					console.log(data2013.length);
-					*/
-					//renderJobPoints(data2012, data2013, fillArr);
 					renderJobPoints(mapPoints, fillArr);
 				}
 			});
 		});
 		
-		$('#btnSearchCompanyGrowthChart').bind('click', function(event){
-			//alert('came here');
+		$('#btnSearchCompanyGrowth').bind('click', function(event){
 			$.ajax({
 				url : window.apiPath + 'getCompanyGrowthOverTime',
 				type : 'POST',
@@ -83,15 +69,12 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 				data : {'term' : $( "#serachCompany").val() },
 				success : function(data) {
 					
-					
+					$("#legend").css("display", "block");
 					var d1 = {
 							name : data['datasets'][0]['label'],
 							label : data['datasets'][0]['label'],
 							fillColor: "#4f99b4",
 							lineColor: "#4f99b4",
-				            /*strokeColor: "rgba(220,220,220,0.8)",
-				            highlightFill: "rgba(220,220,220,0.75)",
-				            highlightStroke: "rgba(220,220,220,1)",*/
 				            data : data['datasets'][0]['data']
 					};
 					var d2 = {
@@ -99,9 +82,6 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 							label : data['datasets'][1]['label'],
 							fillColor: "#d67777",
 							lineColor: "#d67777",
-							/*strokeColor: "rgba(151,187,205,0.8)",
-							highlightFill: "rgba(151,187,205,0.75)",
-							highlightStroke: "rgba(151,187,205,1)",*/
 							data : data['datasets'][1]['data']
 					};
 					
@@ -114,38 +94,6 @@ window.apiPath = window.location.protocol + "//" + window.location.host + "/jobs
 					var myBarChart = new Chart(ctx).Bar({'labels' : data['labels'], 'datasets' : [d1, d2]}, options);
 					
 					
-/*					
-					var jobs = [];
-					for(var key in data){
-						jobs.push(data[key]);
-					}
-					console.log(data);
-					
-					var results = function(){ 
-						return  jobs;
-					};
-					console.log(results());
-					nv.addGraph(function() {
-					    var chart = nv.models.multiBarHorizontalChart()
-					        .x(function(d) { return d.label})
-					        .y(function(d) { return d.value})
-					        .margin({top: 30, right: 20, bottom: 50, left: 175})
-					        .showValues(true)           //Show bar value next to each bar.
-					        .tooltips(true)             //Show tooltips on hover.
-					        .transitionDuration(350)
-					        .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
-
-					    chart.yAxis
-					        .tickFormat(d3.format(',.2f'));
-
-					    d3.select('#chart1 svg')
-					        .datum(results())
-					        .call(chart);
-
-					    nv.utils.windowResize(chart.update);
-					    return chart;
-					});
-					*/
 				}
 			});
 		});
@@ -172,73 +120,10 @@ function renderJobPoints(mapPoints, fillArr) {
 			fills : fillArr,
 			data : {}
 		});
-		//console.log(mapPoints);
 		map.bubbles(mapPoints, {
 			popupOnHover: false,
 	        highlightOnHover: false
 		});
-		/*
-		var width = 1200;
-		var height = 500;			 
-		var projection = d3.geo.equirectangular()
-		      .scale(153)
-		      .translate([width / 2, height / 2])
-		      .precision(.1);
-			 
-		 var map = new Datamap(
-		{
-			element : document.getElementById('container'),
-			geographyConfig : {
-				popupOnHover: true,
-				highlightOnHover : true,
-				popupTemplate : function(geo, data) {
-					return [
-					        '<div class="hoverinfo"><strong>',
-					         + geo.properties.name, '</strong></div>' ]
-					.join('');
-				},
-				projection: ''
-			},
-			setProjection: function(element) {
-	          var projection = d3.geo.equirectangular()
-			      .scale(153)
-			      .translate([width / 2, height / 2])
-			      .precision(.1);
-	          var path = d3.geo.path()
-	            .projection(projection);
-	          
-	          return {path: path, projection: projection};
-	        },
-			fills : fillArr,
-			data : {}
-		}); 
-		
-	   var svg = d3.select("#container svg");
-	  
-	   svg.selectAll("circle")
-		  .data(data2012)
-		  .enter().append("circle")
-		  .attr("r", 1.5)
-		  .style("fill", "red")
-		  .attr("transform", function(d) {
-		    return "translate(" + projection([
-		      d.location.longitude,
-		      d.location.latitude
-		    ]) + ")";
-		  }); 
-	   
-	   svg.selectAll("circle")
-		  .data(data2013)
-		  .enter().append("circle")
-		  .attr("r", 1.5)
-		  .style("fill", "blue")
-		  .attr("transform", function(d) {
-		    return "translate(" + projection([
-		      d.location.longitude,
-		      d.location.latitude
-		    ]) + ")";
-		  }); 
-		  */
 	}
 	
 function getRandomColor() {
